883f97287b6dea9b86c4b5f3b3e2da8c9b0981a7
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-13 18:38:03 -0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 const ImageDisplay = () => {
   const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
   const myImageStyle = { width: '500px', height: '500px' };
 
   return (
+    <MediaQuery minWidth={767}>
     <div>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
       <center>
@@ -12,6 +14,7 @@ const ImageDisplay = () => {
       <img style={myImageStyle} src={imageUrl} alt="Image" />
       </center>
     </div>
+    </MediaQuery>
   );
 };
 
