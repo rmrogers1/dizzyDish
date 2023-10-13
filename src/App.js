75eb6e0f9a36fe158e1460f952187a9e60cffc88
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-12 20:28:41 -0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const ImageDisplay = () => {
+  const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image Display</h1>
+      <img src={imageUrl} alt="Image" />
     </div>
   );
-}
+};
 
-export default App;
+export default ImageDisplay;
