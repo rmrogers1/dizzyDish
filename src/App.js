89a832315ea22ec0d3f75fc0287a6ee463ba448f
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-12 21:01:18 -0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const ImageDisplay = () => {
   const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
+  const myImageStyle = { width: '500px', height: '500px' };
 
   return (
     <div>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
       <center>
       <h1>Welcome to Dizzy Dish</h1>
-      <img src={imageUrl} alt="Image" />
+      <img style={myImageStyle} src={imageUrl} alt="Image" />
       </center>
     </div>
   );
