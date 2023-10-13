ff186e413badcb6999f9925bccfeadcb7730820f
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-12 21:28:22 -0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MediaQuery from 'react-responsive';
 
 const ImageDisplay = () => {
   const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
@@ -9,10 +8,8 @@ const ImageDisplay = () => {
     <div>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
       <center>
-      <MediaQuery maxWidth={1224}>
       <h1>Welcome to Dizzy Dish</h1>
       <img style={myImageStyle} src={imageUrl} alt="Image" />
-      </MediaQuery>
       </center>
     </div>
   );
