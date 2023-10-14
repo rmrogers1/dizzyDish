42e7d7fce85d46414b53e10cde5d81fa587ca30e
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-13 21:25:42 -0500

    new
    new

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dizzydish9819e354": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import './App.css';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+}
+
+from "@aws-amplify/ui-react";
+
+const foodArray = ['Pizza','Sushi','Burgers','Thai','Tacos','Pancakes','Tequila','Worth Wild'];
+//const listItems = foodArray.map((food) =>
+//  <li>{food}</li>
+//);
+//const randomFood = foodArray.length;
+//const theFood = Math.random(5);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 
 const ImageDisplay = () => {
-  const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
+  const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg';
   const myImageStyle = { width: '300px', height: '300px' };
 
   return (
     <div className="App">
       <header className="App-header">
-      <center><h1>Welcome to Dizzy Dish</h1></center>
-      <br></br>
+      <center><h1>Dizzy Dish</h1></center>
       <br></br>
         <img style={myImageStyle} className="App-logo" src={imageUrl} alt="Image" />
+        <h2 className="App-food" >{foodArray[getRandomInt(8)]}</h2>
       </header>
+      
+      
     </div>
+  )  
+}
 
-  );
-};
 
+
+
+
+//export default withAuthenticator(App);
 export default ImageDisplay;
