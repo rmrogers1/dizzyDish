2fcc7d09c47a261512fad42a32f566cf69aa24ab
rmrogers1 <44814100+rmrogers1@users.noreply.github.com>
2023-10-13 19:10:21 -0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import MediaQuery from 'react-responsive';
+import './App.css';
 
 const ImageDisplay = () => {
   const imageUrl = 'https://imagesreact.s3.amazonaws.com/IMG_8655+(1).jpg'; // Replace with your image URL
-  const myImageStyle = { width: '500px', height: '500px' };
+  const myImageStyle = { width: '200px', height: '200px' };
 
   return (
-    <MediaQuery maxWidth={767}>
-    <div>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <center>
-      <h1>Welcome to Dizzy Dish</h1>
-      <img style={myImageStyle} src={imageUrl} alt="Image" />
-      </center>
+    <div className="App">
+      <header className="App-header">
+      <center><h1>Welcome to Dizzy Dish</h1></center>
+      <br></br>
+      <br></br>
+        <img style={myImageStyle} className="App-logo" src={imageUrl} alt="Image" />
+      </header>
     </div>
-    </MediaQuery>
+
   );
 };
 
